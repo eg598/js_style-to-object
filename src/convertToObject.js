c680b3608615c19052061754bb0892ca0081b06a
Yegor <egor598000@gmail.com>
2025-12-09 17:23:58 +0100

    added task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const string = sourceString.split(';');
+  const result = {};
+
+  for (const line of string) {
+    if (!line.includes(':')) {
+      continue;
+    }
+
+    const temp = line.split(':');
+
+    result[temp[0].trim()] = temp[1].trim();
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
